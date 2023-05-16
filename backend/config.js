ea4ec24ad600632c245ae75c32830d9b974c5e8c
Abhishek63977 <ak4811731@gmail.com>
2023-05-16 16:11:11 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+
+const api_config = {
+    PORT : 5000,
+    DB_URL : 'mongodb+srv://ak4811731:abhishek@cluster0.misnnkn.mongodb.net/SportsMania?retryWrites=true&w=majority'
+}
+
+module.exports = api_config;
